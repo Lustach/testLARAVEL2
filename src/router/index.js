58bbf81acfeb8eb1d0938e0aaee9016b3fa594fe
Lustach <46442175+Lustach@users.noreply.github.com>
2020-11-06 17:51:39 +0300

    HM

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import store from '@/store'
+// import store from '@/store'
 
 Vue.use(VueRouter)
 
@@ -47,8 +47,8 @@ const router = new VueRouter({
 	base: process.env.BASE_URL,
 	routes
 })
-router.beforeEach((to, from, next) => {
-	if (to.name !== 'Login' && !store.getters["auth/isLoggedIn"]) next({ name: 'Login' })
-	else next()
-})
+// router.beforeEach((to, from, next) => {
+// 	if (to.name !== 'Login' && !store.getters["auth/isLoggedIn"]) next({ name: 'Login' })
+// 	else next()
+// })
 export default router
